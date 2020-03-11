10ffa337203ae4960efa10928a9593259bcbca74
Emily <emily.a.harber@gmail.com>
2020-03-11 17:13:56 -0400

    get OrdersSerializer working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
 
   def index 
-    orders = Order.all 
-    render json: orders
+    orders = Order.all
+    render json: OrdersSerializer.new(orders).to_serialized_json
   end 
 
   def show 
     order = Order.find_by(id: params[:id])
-    render json: order
+    render json: OrdersSerializer.new(order).to_serialized_json
   end 
 
 end
---
+++ app/services/orders_serializer.rb
@@ -0,0 +1,9 @@
+class OrdersSerializer 
+  def initialize(order_object)
+    @order = order_object 
+  end 
+
+  def to_serialized_json 
+    @order.as_json(except: [:created_at, :updated_at, :doctor_id], include: { doctor: { only: [:name, :specialty]}})
+  end 
+end 
