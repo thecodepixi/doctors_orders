ec0c74b33b0de95513915129186327a4261f790d
Emily <emily.a.harber@gmail.com>
2020-03-11 17:22:02 -0400

    add Order create action for future use

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,4 +10,22 @@ class OrdersController < ApplicationController
     render json: OrderSerializer.new(order).to_serialized_json
   end 
 
+  def create 
+    order = Order.new(
+      appointment_date: params[:appointment_date],
+      appointment_type: params[:appointment_type],
+      test_results: params[:test_results],
+      treatment_info: params[:treatment_info],
+      follow_up: params[:follow_up],
+    )
+    doctor = Doctor.find_or_create_by(name: params[:doctor_name].strip.downcase, specialty: params[:specialty].strip.downcase)
+    order.doctor = doctor 
+    
+    if order.save 
+      render json: OrderSerializer.new(order).to_serialized_json
+    else 
+      render json: { error_message: order.errors.full_messages }
+    end 
+  end 
+
 end
